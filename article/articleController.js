8988ca6671c851551704cc5d34eb43f33b49274d
Vinicius <vinidesenvolve@gmail.com>
2021-01-10 22:05:12 -0300

    Criaçao formulario de artigo

--- article/articleController.js
+++ article/articleController.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const Category = require("../category/Category");
 
 router.get("/articles", (req, res) =>{
     res.send("Articles");
 });
 
 router.get("/admin/articles/new", (req, res) =>{
-    res.send("Add new article");
+    Category.findAll().then(categories =>{
+        res.render("../views/admin/articles/new", {categories: categories});
+    });
 });
 
 module.exports = router;
