902869b47279bba5d1123a5861df4c9eefbfaa41
vinidesenvolve <vinidesenvolve@gmail.com>
2021-04-25 13:19:55 -0300

    Implementando User model e userController

---
+++ user/User.js
@@ -0,0 +1,21 @@
+const Sequelize = require("sequelize");
+const connection = require("../database/database");
+
+const User = connection.define('users', {
+    email: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    password: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+});
+
+User.sync({force: false});
+
+module.exports = User;
